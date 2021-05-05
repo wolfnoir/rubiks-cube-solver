661d8bd322c328a79c6b82fbf4aefd3c2a561046
Everett Yang <amber.yang@stonybrook.edu>
2021-05-05 18:30:37 -0400

    Added PyCuber

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+import pycuber as pc
+from pycuber.solver import CFOPSolver
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Create a Cube object
+    mycube = pc.Cube()
+
+    #random algorithm
+    random = pc.Formula().random()
+
+    # Do something at the cube.
+    mycube(random)
+
+    print(mycube)
