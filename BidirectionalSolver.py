44b7e9871aaa9b6eeab2410967558a445605a074
AndyZheng430 <zhenga6@bxscience.edu>
2021-05-06 22:10:56 -0400

    created a BFS algorithm that is missing goal checking and creating new nodes

--- BidirectionalSolver.py
+++ BidirectionalSolver.py
@@ -1,1 +1,24 @@
 # TODO: bidirectional search for solving the rubiks cube
+from util import cNode
+
+class BDS():
+
+    def __init__(self):
+        self.explored = list()
+
+    def BFS(self, startNode):
+        frontier = list()
+        goal = False
+        if goal: # perform goal check
+            return startNode
+        frontier.append(startNode)
+        while frontier != []: 
+            currentNode = frontier.pop(0)
+            self.explored.append(currentNode)
+            for action in currentNode.actions:
+                newNode = None # create new rubix cube
+                if newNode not in frontier and newNode not in self.explored: # check if value of the rubix cube is the same
+                    if goal: # perform goal check
+                        return newNode
+                    frontier.append(newNode)
+
--- util.py
+++ util.py
@@ -1,3 +1,13 @@
 class cNode():
+    """
+    Parameters:
+        cube: current state of the rubix cube
+        prev: previous node with previous rubix cube state
+        next: next node with previous rubix cube state
+        actions: a list of available actions to take
+    """
     def __init__(self, cube):
         self.cube = cube
+        self.prev = None
+        self.next = None
+        self.actions = []
