ed4683b4fc3927e8160f957aade360ac8524c2bd
Everett Yang <amber.yang@stonybrook.edu>
2021-05-08 23:20:05 -0400

    Added more to RBFS solver

--- RBFSSolver.py
+++ RBFSSolver.py
@@ -10,23 +10,40 @@ import pycuber
 def rbfs_search(start, successors, state_value, is_goal, f_limit):
     if is_goal(start):
         return [start, 0]
+    # change g to be dynamic....this is not correcet
     g = 1
     h = state_value(start)
     f = g + h
     # get successors from start state
+    successors_stuff = successors(start, [])
     # if successors is empty, return failure and arbitrarily large f-limit
     if not successors:
         return [-1, 999999]
-
+    successors_f_values = []
+    i = 0
     # for each s in successors
-        # update f with value from previous search
-        # s.f <-- max(s.g + s.h, node.f)
-    # loop do
+    for (action, state) in successors_stuff:
+        print(state)
+        new_f = state_value(state) + 1
+        max_f = max(new_f, f)
+        temp = (max_f, i)
+        successors_f_values.append(temp)
+
+    while True:
+        # sort successors by f-value
+        sorted(successors_f_values, key=lambda successor: successor[0])
         # best <-- lowest f-value in successors
-        # if best.f > f-limit, return [failure, best.f]
+        best = successors_f_values[0]
+        successor_index = best[1]
+        if best[0] > f_limit:
+            return [-1, best[0]]
         # alternative <-- second lowest f-value among successors
-        # result, best.f <-- RBFS(start, successors, state_value,
+        alternative = successors_f_values[1]
+        # result, best.f <-- CALL RBFS HERE
+        result = 0
         # if result != failure, return result
+        if result[0] != -1:
+            return result
 
 
 class RBFSSolver(object):
