bb28221a79507ac65d4d4055a202861474fb1578
AndyZheng430 <zhenga6@bxscience.edu>
2021-05-10 13:37:40 -0400

    created new test files with 5 moves

--- RubikCubeGenerator.py
+++ RubikCubeGenerator.py
@@ -5,10 +5,18 @@ def createTestFile(size):
     for i in range(0, size):
         filename = "test" + str(i)
         randomFormula = str(pycuber.Formula().random())
-        # print(randomFormula)
         with open(filename, "w") as writer:
             writer.write(randomFormula)
     
+def createTestFile5(size):
+    for i in range(0, size):
+        filename = "test" + str(i)
+        randomFormula = str(pycuber.Formula().random())
+        formulaArr = randomFormula.split(" ")[:5]
+        randomFormula = " ".join(formulaArr)
+        with open(filename, "w") as writer:
+            writer.write(randomFormula)
+
 if __name__ == "__main__":
     number = int(sys.argv[1])
     createTestFile(number)
